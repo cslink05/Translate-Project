0da998c0ad0259a7eb4f02097691d1f8037d4b39
unknown <c.s.link05@gmail.com>
2019-01-13 01:44:24 +0900

    completed app with functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from '../components/LanguageSelector';
 
 class App extends React.Component {
+    state = {language: 'english' };
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    };
+
     render() {
-        return <div>App</div>;
+        return (
+            <div className="ui container">
+            <LanguageStore>
+                <LanguageSelector />
+                    <ColorContext.Provider value="primary">
+                            <UserCreate />
+                    </ColorContext.Provider>
+                </LanguageStore>
+            </div>
+        );
     }
 }
 
